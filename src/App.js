f5bf67c19adc16599fdde04000b11e486c61ad7e
Tristan <ttrosvig804@gmail.com>
2021-01-28 14:13:26 -0600

    once again

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	const message = process.env.REACT_APP_MESSAGE || 'failed';
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
-					<p>{message}</p>
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
